aebd3965a9d7727c6af340dd00dca95ac9426c83
Anushka <anugsawant@gmail.com>
2026-08-16 14:56:37 +0530

    third task user comp

---
+++ 03-team-directory-dashboard/src/User.jsx
@@ -0,0 +1,12 @@
+function User({ user, handleUserClick }) {
+  return (
+    <div className="user" onClick={() => handleUserClick(user)}>
+      <h2>{user.name}</h2>
+      <p>{user.company.name}</p>
+      <p>{user.email}</p>
+      <p>{user.address.city}</p>
+    </div>
+  );
+}
+ 
+export default User;
